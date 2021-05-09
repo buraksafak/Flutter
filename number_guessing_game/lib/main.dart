54821627a3679e6836e36672b9595f0feb9f0363
buraksafak <buraksafak2109@gmail.com>
2021-05-09 15:10:57 +0200

    Refactoring project

--- number_guessing_game/lib/main.dart
+++ number_guessing_game/lib/main.dart
@@ -35,51 +35,71 @@ class _HomePageState extends State<HomePage> {
           title: Text("Home Page"),
         ),
         body: Center(
-            child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: screenWidth/20, right: screenWidth/20),
-                child: Text(
-                  "Guess Game",
-                  style: TextStyle(
-                    color: Colors.orangeAccent,
-                    fontSize: screenWidth / 10,
-                  ),
-                ),
-              ),
-              Container(
-                height: screenHeight/3,
-                  padding: EdgeInsets.only(top: screenHeight/20),
-                  child: Image.asset("images/number_guess.png")
-              ),
-              Container(
-                padding: EdgeInsets.only(top: screenHeight / 20),
-                height: screenHeight / 7,
-                width: screenWidth / 2,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0))),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.greenAccent),
-                  ),
-                  child: Text(
-                    "Start Game",
-                    style: TextStyle(
-                        color: Colors.deepOrange, fontSize: 20),
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => GuessScreen()));
-                  },
-                ),
-              )
-            ],
-          ),
-        )
+            child: buildSingleChildScrollView(screenWidth, screenHeight, context)
         )
     );
+  }
+
+  SingleChildScrollView buildSingleChildScrollView(double screenWidth, double screenHeight, BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+          children: [
+            buildGuessGameTextContainer(screenWidth),
+            buildGuessImageContainer(screenHeight),
+            buildStartGameButtonContainer(screenHeight, screenWidth, context)
+          ],
+        ),
+      );
+  }
+
+  Container buildStartGameButtonContainer(double screenHeight, double screenWidth, BuildContext context) {
+    return Container(
+              padding: EdgeInsets.only(top: screenHeight / 20),
+              height: screenHeight / 7,
+              width: screenWidth / 2,
+              child: buildStartGameElevatedButton(context),
+            );
+  }
+
+  ElevatedButton buildStartGameElevatedButton(BuildContext context) {
+    return ElevatedButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0))),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.greenAccent),
+              ),
+              child: Text(
+                "Start Game",
+                style: TextStyle(
+                    color: Colors.deepOrange, fontSize: 20),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => GuessScreen()));
+              },
+            );
+  }
+
+  Container buildGuessImageContainer(double screenHeight) {
+    return Container(
+              height: screenHeight/3,
+                padding: EdgeInsets.only(top: screenHeight/20),
+                child: Image.asset("images/number_guess.png")
+            );
+  }
+
+  Container buildGuessGameTextContainer(double screenWidth) {
+    return Container(
+              padding: EdgeInsets.only(left: screenWidth/20, right: screenWidth/20),
+              child: Text(
+                "Guess Game",
+                style: TextStyle(
+                  color: Colors.orangeAccent,
+                  fontSize: screenWidth / 10,
+                ),
+              ),
+            );
   }
 }
